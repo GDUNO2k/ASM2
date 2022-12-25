f9b84ed73a4ff506e8fd0808ae4a055fd2fcd9dc
GDUNO2k <zzhaduongprozz@gmail.com>
2022-12-25 13:39:35 +0700

    full

--- model/db.js
+++ model/db.js
@@ -2,12 +2,11 @@ const MongoClient = require('mongodb').MongoClient
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  "mongodb+srv://duongvabop2000:17082000@cluster0.hidub.mongodb.net/Database?retryWrites=true&w=majority",
-   { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(()=> {
-    console.log('Database connected');
-  })
-  .catch((error)=> {
-    console.log('Error connecting to database : ' + error);
-  });
-  require('./product.model');
+        "mongodb+srv://admin:admin@cluster0.8mgr7qd.mongodb.net/test", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Database connected');
+    })
+    .catch((error) => {
+        console.log('Error connecting to database : ' + error);
+    });
+require('./product.model');
